685b57ca16f4cfcd8c7311b18684a7bbfe3a1c9d
NipunSGeeTH <sangeethnipun385@gmail.com>
2026-01-12 20:52:54 +0530

    fix:dist build issue

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Main export file - exports all traditional date formatters
+
+// Sinhala formatter
+export { formatSinhala } from './packages/sinhala/index.js';
+
+// Placeholder exports for future formatters
+// export { formatNepali } from './packages/nepali/index.js';
+// export { formatPersian } from './packages/persian/index.js';
+// export { formatEthiopian } from './packages/ethiopian/index.js';
+// export { formatChinese } from './packages/chinese/index.js';
--- packages/sinhala/index.ts
+++ packages/sinhala/index.ts
@@ -5,7 +5,7 @@ import {
   MONTH_NAMES, 
   GREGORIAN_MONTHS,
   toSinhalaNumber 
-} from './data.ts';
+} from './data.js';
 
 function getTraditionalMonth(gregorianMonth: number) {
   const mapping = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
